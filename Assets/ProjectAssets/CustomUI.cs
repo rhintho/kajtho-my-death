b7cfd25ec650a410be395e66fe49e618ddca5912
Kathrin Konkol <konkol.k@live.de>
2019-01-04 11:36:11 +0100

    init commit

---
+++ Assets/ProjectAssets/CustomUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomUI : MonoBehaviour {
+    float speed = 0.1f;
+    public GameObject[] openedApps;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+            // Get movement of the finger since last frame
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+            if (touchDeltaPosition.x > 1) {
+                SwipeRight();
+            }
+
+        }
+    }
+
+    void SwipeRight() {
+        Debug.Log("swipe right");
+    }
+
+    public void CloseApps() {
+        openedApps = GameObject.FindGameObjectsWithTag("App");
+
+        foreach(GameObject app in openedApps){
+            app.SetActive(false);
+        }
+
+    }
+}
